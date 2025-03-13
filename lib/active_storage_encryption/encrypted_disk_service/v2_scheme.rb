e3a702fbb427290408ea931f2a15593314fa7d10
Julik Tarkhanov <me@julik.nl>
2025-03-13 10:18:56 +0000

    Revert v2 scheme
    When making these changes we need to make sure the already encrypted files do not break

--- lib/active_storage_encryption/encrypted_disk_service/v2_scheme.rb
+++ lib/active_storage_encryption/encrypted_disk_service/v2_scheme.rb
@@ -4,9 +4,8 @@
 # at the end of the message. The message is prefixed by a SHA2 digest of the encryption key.
 class ActiveStorageEncryption::EncryptedDiskService::V2Scheme
   def initialize(encryption_key)
-    truncated_key = encryption_key.byteslice(0, 32)
-    @scheme = BlockCipherKit::AES256GCMScheme.new(truncated_key)
-    @key_digest = Digest::SHA256.digest(truncated_key)
+    @scheme = BlockCipherKit::AES256GCMScheme.new(encryption_key)
+    @key_digest = Digest::SHA256.digest(encryption_key.byteslice(0, 32)) # In this scheme just the key is used
   end
 
   def streaming_decrypt(from_ciphertext_io:, into_plaintext_io: nil, &blk)
